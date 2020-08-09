e6ea70f4e078843756522a074064c9c756f15f53
SKP <skp.frl@gmail.com>
2020-08-09 23:00:52 +0530

    refactor(rot): move constants like MAX and MIN to a separate inner module

---
+++ src/rot/constants.rs
@@ -0,0 +1,4 @@
+// These constants may be set during runtime at a later date. Something like a config argument
+// for specifying the range of ASCII characters to encrypt / decrypt would be a desirable feature.
+pub const MAX: u8 = 126;
+pub const MIN: u8 = 33;
