7c21b8d58cc387aba7dca70841dc62f2934c203d
SKP <skp.frl@gmail.com>
2020-08-10 23:57:27 +0530

    feat(rot): add parse_to_path utility function and write tests for it

--- src/rot/utils.rs
+++ src/rot/utils.rs
@@ -2,6 +2,13 @@
 use std::ffi::OsStr;
 use std::path::Path;
 
+/// Gives back the output directory path to the new transformed file.
+/// `instance_path` signifies the original path to the file that was encrypted / decrypted.
+pub fn parse_to_path(outdir: &str, filename: &str, instance_path: &Path) -> String {
+  let outpath = format!("{}{}.{}", outdir, filename, get_extension(instance_path));
+  outpath
+}
+
 /// Returns the extension of a path as a string slice.
 /// # Examples
 /// ```
@@ -9,7 +16,7 @@ use std::path::Path;
 /// let ext = get_extension(p);
 /// println!("Extension of some_file is {}", ext);
 /// ```
-pub fn get_extension(p: &Path) -> &str {
+fn get_extension(p: &Path) -> &str {
   let ext = p.extension().and_then(OsStr::to_str);
 
   match ext {
@@ -30,5 +37,15 @@ mod tests {
     let path = Path::new("some_file.txt");
     let ext = get_extension(path);
     assert_eq!(ext, "txt");
+  }
+
+  #[test]
+  fn it_parses_to_path() {
+    let instance_path = Path::new("how to make pineapple on pizza (eww).txt");
+    // This translates to a directory path of: "f/(TOP SECRET) recipes/forbidden recipe.txt"
+    let outdir = "f/(TOP\\ SECRET)\\ recipes/";
+    let filename = "forbidden\\ recipe";
+    let output = parse_to_path(outdir, filename, instance_path);
+    assert_eq!(output, "f/(TOP\\ SECRET)\\ recipes/forbidden\\ recipe.txt")
   }
 }
