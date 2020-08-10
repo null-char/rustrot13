25ce4b1e901a44b33747bdc07892b3c2dd52a1ba
SKP <skp.frl@gmail.com>
2020-08-10 23:58:39 +0530

    feat(rot): add decryptor

--- src/rot/decryptor.rs
+++ src/rot/decryptor.rs
@@ -1,8 +1,9 @@
 use super::{
   constants::{MAX, MIN},
   traits::ByteRotator,
-  utils::get_extension,
+  utils::parse_to_path,
 };
+use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::path::Path;
 
@@ -23,8 +24,46 @@ impl<'p> FileDecryptor<'p> {
     }
   }
 
-  // TODO: Implement decrypt_file method on FileDecryptor.
-  pub fn decrypt_file(outdir: &str, filename: &str) {}
+  pub fn decrypt_file(&self, outdir: &str, filename: Option<&str>) {
+    let filename = filename.unwrap_or("decrypted");
+
+    match File::open(self.path) {
+      Ok(file) => {
+        let buf = BufReader::new(file);
+        let mut decrypted_bytes: Vec<u8> = vec![];
+
+        for byte in buf.bytes() {
+          // Stop execution if reading one of the bytes failed.
+          if let Err(_) = byte {
+            println!("error: Reading bytes from file for decryption failed");
+            return;
+          }
+
+          let rotated_byte = self.rotate_byte(byte.unwrap());
+          decrypted_bytes.push(rotated_byte);
+        }
+
+        // Determine the directory at which to create the output file
+        let outpath = parse_to_path(outdir, filename, self.path);
+        let outpath = outpath.as_str();
+
+        match File::create(outpath) {
+          Ok(mut file) => {
+            let buf = decrypted_bytes.as_slice();
+            match file.write_all(buf) {
+              Ok(()) => println!("Successfully wrote the decrypted file to {}", outpath),
+              Err(_) => println!("error: Writing to decrypted file failed"),
+            }
+          }
+          Err(err) => println!(
+            "error: Creation of decrypted file failed due to err: {:?}",
+            err
+          ),
+        }
+      }
+      Err(_) => println!("error: File for decryption couldn't be opened"),
+    }
+  }
 }
 
 impl ByteRotator for FileDecryptor<'_> {
@@ -41,5 +80,37 @@ impl ByteRotator for FileDecryptor<'_> {
     }
 
     byte
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{super::constants::ROT_BY, *};
+  use std::fs::File;
+  use std::io::{BufReader, Error, Read};
+  use std::path::Path;
+
+  #[test]
+  fn it_decrypts_file() {
+    let fd = FileDecryptor::new(Path::new("test2.txt"), ROT_BY);
+    fd.decrypt_file("", None);
+
+    match File::open("decrypted.txt") {
+      Ok(file) => {
+        let buf = BufReader::new(file);
+        let results = buf.bytes().collect::<Vec<Result<u8, Error>>>();
+        let bytes_vec: Vec<u8> = results
+          .into_iter()
+          .map(|b| match b {
+            Ok(b) => return b,
+            Err(err) => panic!("Error mapping results: {:?}", err),
+          })
+          .collect();
+
+        let result_str = String::from_utf8(bytes_vec).unwrap();
+        assert_eq!(result_str, String::from("This is for the decryption test."));
+      }
+      Err(_) => panic!("Failed to open decrypted file"),
+    }
   }
 }
